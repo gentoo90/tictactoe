2cec1bad1c911a8f081a88f1c7a971fefd8ca8cf
gentoo90 <gentoo90@gmail.com>
2011-10-24 22:21:23 +0300

    work on game

--- tictactoe/game/views.py
+++ tictactoe/game/views.py
@@ -1,1 +1,29 @@
-# Create your views here.
+from django.http import HttpResponse
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+import pusher
+
+PUSHER = {
+    'APP_ID':'9983',
+    'KEY':'bc9867d3dadaea0b8a27',
+    'SECRET':'ac1dba415b21fde5245e'
+}
+
+pusher.app_id = PUSHER['APP_ID']
+pusher.key = PUSHER['KEY']
+pusher.secret = PUSHER['SECRET']
+p = pusher.Pusher()
+
+def page_x(request):
+    return render_to_response('game.html',dict({'me':'x', 'enemy':'o'}.items() + PUSHER.items()))
+
+def page_o(request):
+    return render_to_response('game.html',dict({'me':'x', 'enemy':'o'}.items() + PUSHER.items()))
+
+def turn_x(request):
+    p['turn_x'].trigger('turn_is_done', request.POST.get('cell', ''))
+    return HttpResponse('OK')
+
+def turn_o(request):
+    p['turn_o'].trigger('turn_is_done', request.POST.get('cell', ''))
+    return HttpResponse('OK')
--- tictactoe/urls.py
+++ tictactoe/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls.defaults import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^game/x/$', 'tictactoe.game.views.page_x'),
+    url(r'^game/o/$', 'tictactoe.game.views.page_o'),
+    url(r'^turn/x/$', 'tictactoe.game.views.turn_o'),
+    url(r'^turn/o/$', 'tictactoe.game.views.turn_o'),
     # Examples:
     # url(r'^$', 'tictactoe.views.home', name='home'),
     # url(r'^tictactoe/', include('tictactoe.foo.urls')),
@@ -13,5 +17,5 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
